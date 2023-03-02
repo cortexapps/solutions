a21635b69d159e1899a8831e8f4e90587dece775
cremerfc <cremerfc@users.noreply.github.com>
2023-03-02 16:18:38 -0500

    Removed API Token

--- public-api/getting_started/add-packages.py
+++ public-api/getting_started/add-packages.py
@@ -1,7 +1,7 @@
 import requests
 
 cortex_tag = 'api-created-svc'
-api_token = 'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAABWLwQrCMAxA_yXnFeKWJu1uHnbwJornEWOFwlgHm6KI_249vsd7H1gfV-ihJYQGtveSKuyPh_FyHk7VWJmmvOYyj_os-aaz_QOlzhg7da1ndnQXcVGSOR-DhBSNPFmds27Q71iYoqCnBtJrqUIwVELk7w_9R3-pfgAAAA.5PYN5rV1ILVHvH5wjBPUK7LrgJJqY7-8pvaTMIMMObAQ7eNM83XjAyK_l5yXIgZlaNcDmxlxG9I9bagFj55JEw'
+api_token = ''<your-api-token-goes-here>''
 api_url = 'https://api.getcortexapp.com/api/v1/catalog/' + cortex_tag + '/packages/python/requirements'
 headers = {
       'Authorization': 'Bearer ' + api_token,

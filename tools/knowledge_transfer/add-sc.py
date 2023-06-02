462ce9b6bd3bf3687ccdb377c77e187350d70db6
cremerfc <cremerfc@users.noreply.github.com>
2023-06-02 15:43:22 -0400

    Update add-sc.py
    Added logic to only process files with `yml` extension

--- tools/knowledge_transfer/add-sc.py
+++ tools/knowledge_transfer/add-sc.py
@@ -1,4 +1,3 @@
-# script to add scorecards from a folder
 import os
 import requests
 import json
@@ -16,19 +15,20 @@ else:
     #Let's open the 'yamls' directory and look for scorecards
     for filename in os.listdir('yamls'):
         full_path = os.path.join('yamls', filename)
-        print("Processing file " + full_path)
-        with open(full_path, 'r') as stream:
-            sc_file = yaml.safe_load(stream)
-            #for some reason the above loads it as JSON so we need to transform it to yml
-            sc_yaml = yaml.dump(sc_file)
-            #print("The Contents of " + full_path + " are " + sc_yaml)
-            #Now we can upload the scorecard to the destination instance
-            u_url = c_url + 'api/v1/scorecards/descriptor'
-            u_headers = {
+        if (full_path.endswith('.yml')):
+            print("Processing file " + full_path)
+            with open(full_path, 'r') as stream:
+                sc_file = yaml.safe_load(stream)
+                #for some reason the above loads it as JSON so we need to transform it to yml
+                sc_yaml = yaml.dump(sc_file)
+                #print("The Contents of " + full_path + " are " + sc_yaml)
+                #Now we can upload the scorecard to the destination instance
+                u_url = c_url + 'api/v1/scorecards/descriptor'
+                u_headers = {
                     'Content-Type': 'application/yaml;charset=UTF-8',
                     'Accept': 'application/json',
                     'Authorization': 'Bearer ' + c_token
                 }
-            u_response = requests.post(u_url, headers=u_headers, data=sc_yaml)
-            print(u_response.text)        
+                u_response = requests.post(u_url, headers=u_headers, data=sc_yaml)
+                print(u_response.text)        
         
